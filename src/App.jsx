3c82c8360ad2c955e7a4fb6b903d77c515f0129b
Nacho Miranda <166954662+champagneplease@users.noreply.github.com>
2024-12-05 12:24:47 -0300

    Add title

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,22 @@ import {
   MDBIcon,
   MDBRow,
   MDBTypography,
-
 } from "mdb-react-ui-kit";
 import WeatherCard from "./components/WeatherCard";
 
 export default function App() {
   return (
-    <section className="vh-100" style={{ backgroundColor: "#f5f6f7" }}>
-      <MDBContainer className="h-100">
-        <MDBRow className="justify-content-center align-items-center h-100">
-          <MDBCol md="10" lg="8" xl="6">
-            <WeatherCard />
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-    </section>
+    <>
+      <h1>Weather App</h1>
+      <section className="vh-100" style={{ backgroundColor: "#f5f6f7" }}>
+        <MDBContainer className="h-100">
+          <MDBRow className="justify-content-center align-items-center h-100">
+            <MDBCol md="10" lg="8" xl="6">
+              <WeatherCard />
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
+    </>
   );
 }
